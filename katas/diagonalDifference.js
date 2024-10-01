04d38743d80511222e388dc207ccc2b60103d28d
nil-e <154754621+nil-e@users.noreply.github.com>
2024-10-01 14:21:39 +0200

    Update diagonalDifference.js

--- katas/diagonalDifference.js
+++ katas/diagonalDifference.js
@@ -1,35 +1,35 @@
-Question:
-Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// Question:
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
-For example, the square matrix  is shown below:
+// For example, the square matrix  is shown below:
 
-1 2 3
-4 5 6
-9 8 9  
-The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+// 1 2 3
+// 4 5 6
+// 9 8 9  
+// The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
 
-Function description
+// Function description
 
-Complete the  function in the editor below.
+// Complete the  function in the editor below.
 
-diagonalDifference takes the following parameter:
+// diagonalDifference takes the following parameter:
 
-int arr[n][m]: an array of integers
-Return
+// int arr[n][m]: an array of integers
+// Return
 
-int: the absolute diagonal difference
-Input Format
+// int: the absolute diagonal difference
+// Input Format
 
-The first line contains a single integer, , the number of rows and columns in the square matrix .
-Each of the next  lines describes a row, , and consists of  space-separated integers .
+// The first line contains a single integer, , the number of rows and columns in the square matrix .
+// Each of the next  lines describes a row, , and consists of  space-separated integers .
 
-Constraints
+// Constraints
 
-Output Format
+// Output Format
 
-Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+// Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
 
-Solution:
+// Solution:
 function diagonalDifference(arr) {
     let matrixDim = arr.length;
     let sumOne = 0;
