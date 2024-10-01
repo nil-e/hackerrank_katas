040957d483e50eda5b61fe7a3e00311d03ce0da5
nil-e <154754621+nil-e@users.noreply.github.com>
2024-10-01 14:21:48 +0200

    Update simpleArraySum.js

--- katas/simpleArraySum.js
+++ katas/simpleArraySum.js
@@ -1,28 +1,28 @@
-Question:
-Given an array of integers, find the sum of its elements.
+// Question:
+// Given an array of integers, find the sum of its elements.
 
-For example, if the array , , so return .
+// For example, if the array , , so return .
 
-Function Description
+// Function Description
 
-Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
 
-simpleArraySum has the following parameter(s):
+// simpleArraySum has the following parameter(s):
 
-ar: an array of integers
-Input Format
+// ar: an array of integers
+// Input Format
 
-The first line contains an integer, , denoting the size of the array.
-The second line contains  space-separated integers representing the array's elements.
+// The first line contains an integer, , denoting the size of the array.
+// The second line contains  space-separated integers representing the array's elements.
 
-Constraints
+// Constraints
 
 
-Output Format
+// Output Format
 
-Print the sum of the array's elements as a single integer.
+// Print the sum of the array's elements as a single integer.
 
-Solution:
+// Solution:
 function simpleArraySum(ar) {
     const initialValue = 0;
     const sumArr = ar.reduce((accumulator,currenVal)=> {
